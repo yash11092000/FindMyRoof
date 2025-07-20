59722ca5ac62542255aca3829c382e3f89b28388
nikita kasab <nikitakasab8989@gmail.com>
2025-07-20 18:25:38 +0530

    Nikita_Added some master Models 20/07/2025

---
+++ PhysioWeb/Models/AreaMaster.cs
@@ -0,0 +1,17 @@
+﻿namespace PhysioWeb.Models
+{
+    public class AreaMaster : CommanProp
+    {
+        public string AreaName { get; set; }
+        public string Pincode { get; set; }
+        public string LatitudeLongitude { get; set; }
+        public bool IsActive { get; set; }
+        public List<DropDownSource> State { get; set; }
+        public List<DropDownSource> City { get; set; }
+        public List<DropDownSource> Country { get; set; }
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public int CountryID { get; set; }
+
+    }
+}
---
+++ PhysioWeb/Models/FurnishingTypeMaster.cs
@@ -0,0 +1,12 @@
+﻿namespace PhysioWeb.Models
+{
+    public class FurnishingTypeMaster : CommanProp
+    {
+        public int FurnishingTypeID { get; set; }
+        public string FurnishingType { get; set; }
+        public bool IsActive { get; set; }
+    }
+
+
+
+}
