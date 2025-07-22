5c16a7140df6da52ce2224a9f7677d83fff7bf56
nikita kasab <nikitakasab8989@gmail.com>
2025-07-22 22:42:25 +0530

    Nikita_22/07/2025

--- PhysioWeb/Models/AmenityMaster.cs
+++ PhysioWeb/Models/AmenityMaster.cs
@@ -2,11 +2,8 @@
 {
     public class AmenityMaster : CommanProp
     {
-        public int AmenityID { get; set; }
         public string AmenityName { get; set; }
         public string IconImage { get; set; } 
         public bool IsActive { get; set; }
     }
-
-
 }
