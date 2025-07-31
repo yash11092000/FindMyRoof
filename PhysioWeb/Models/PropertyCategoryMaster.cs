138e517973cc04b0ccb33f10f0a1e8c55772847e
nikita kasab <nikitakasab8989@gmail.com>
2025-07-31 18:13:03 +0530

    Nikita_31/07/2025

--- PhysioWeb/Models/PropertyCategoryMaster.cs
+++ PhysioWeb/Models/PropertyCategoryMaster.cs
@@ -1,9 +1,49 @@
-﻿namespace PhysioWeb.Models
+﻿using System.Data;
+
+namespace PhysioWeb.Models
 {
     public class PropertyCategoryMaster : CommanProp
     {
         public string CategoryName { get; set; }
         public bool IsActive { get; set; }
+        public string InActiveText { get; set; }
+        public PropertyCategoryMaster()
+        {
+
+        }
+
+        public PropertyCategoryMaster(IDataReader reader, int flag = 0)
+        {
+            if (flag == 0)
+            {
+                TotalCount = reader["TotalCount"] != DBNull.Value ? Convert.ToInt32(reader["TotalCount"]) : 0;
+                UniquId = Convert.ToInt32(reader["UniquId"]);
+                CategoryName = reader["PropertyCategoryName"].ToString();
+                InActiveText = reader["IsActive"].ToString();
+                CreatedBy = reader["CreatedBy"].ToString();
+            }
+            else if (flag == 1)
+            {
+                populateObject(this, reader);
+            }
+        }
+        private void populateObject(PropertyCategoryMaster obj, IDataReader rdr)
+        {
+            if (!rdr.IsDBNull(rdr.GetOrdinal("UniqueID")))
+            {
+                obj.UniquId = rdr.GetInt32(rdr.GetOrdinal("UniqueID"));
+            }
+            if (!rdr.IsDBNull(rdr.GetOrdinal("PropertyCategoryName")))
+            {
+                obj.CategoryName = rdr.GetString(rdr.GetOrdinal("PropertyCategoryName"));
+            }
+            
+            if (!rdr.IsDBNull(rdr.GetOrdinal("IsActive")))
+            {
+                obj.IsActive = rdr.GetBoolean(rdr.GetOrdinal("IsActive"));
+            }
+
+        }
     }
 
 }
--- PhysioWeb/Repository/IMasterRepository.cs
+++ PhysioWeb/Repository/IMasterRepository.cs
@@ -5,6 +5,9 @@ namespace PhysioWeb.Repository
     public interface IMasterRepository
     {
         Task<bool> SavePropCategory(PropertyCategoryMaster propertyCategoryMaster);
+        Task<DataTableResult> ListPropertyCategory(DataTablePara dataTablePara);
+        Task<PropertyCategoryMaster> EditPropertyCategory(int UniqueID, int UserID);
+        Task<bool> DeletePropertyCategory(PropertyCategoryMaster PropertyCategoryMaster);
         Task<bool> SavePropType(PropertyTypeMaster PropertyTypeMaster);
         Task<bool> DeletePropertyType(PropertyTypeMaster PropertyTypeMaster);
         Task<DataTableResult> ListPropertyType(DataTablePara dataTablePara);
